9930b394dac5c1a0fb2af51bdc4d936edab0f841
antnebulaa <adrienlc@adriens-macbook-pro.home>
2025-12-12 15:18:00 +0100

    style: set mobile horizontal padding to 10px

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -48,7 +48,7 @@ const HomeClient: React.FC<HomeClientProps> = ({
                     overflow-y-auto 
                     scrollbar-hide
                 ">
-                    <div className="pt-6 px-4 md:pl-6 md:pr-3 pb-32 md:pb-6">
+                    <div className="pt-6 px-[10px] md:pl-6 md:pr-3 pb-32 md:pb-6">
                         <div className="
                             grid 
                             grid-cols-1 
