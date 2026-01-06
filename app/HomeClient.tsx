3d3cb668629f4acaf3ad9f5ff1631d9ac08e8c5d
antnebulaa <adrienlc@adriens-macbook-pro.home>
2026-01-06 23:51:12 +0100

    style: Fix double top padding on desktop homepage

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -161,7 +161,7 @@ const HomeClient: React.FC<HomeClientProps> = ({
             ">
                 {/* Left Column: List + Footer */}
                 <div className={listColumnClasses}>
-                    <div className="pt-20 md:pt-24 px-[10px] md:pl-6 md:pr-3 pb-32 md:pb-6">
+                    <div className="pt-20 md:pt-4 px-[10px] md:pl-6 md:pr-3 pb-32 md:pb-6">
 
                         {!isSearchActive && (
                             <ResumeSearch />
